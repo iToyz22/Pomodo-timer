54af2e31e30201578734bd1fba1327667c0011ef
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-05-29 22:36:40 +0300

    timer in second

--- Pomodoro timer/ViewController.swift
+++ Pomodoro timer/ViewController.swift
@@ -8,12 +8,139 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let lessonLabel : UILabel = {
+      let label = UILabel()
+        label.text = "Pomodoro timer"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let shapeView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "elipse")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let timerLabel : UILabel = {
+      let label = UILabel()
+        label.text = "30"
+        label.font = UIFont.boldSystemFont(ofSize: 84)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let startButton : UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 20
+        button.setTitle("START", for: .normal)
+        button.backgroundColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var timer = Timer()
+    
+    let shapeLayer = CAShapeLayer()
+    
+    var durationTimer = 30
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.animationCircular()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setConstraints()
+        
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
     }
-
-
+    @objc func startButtonTapped() {
+        basicAnimation()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
 }
+    
+    @objc func timerAction() {
+        
+        durationTimer -= 1
+        timerLabel.text = "\(durationTimer)"
+        print(durationTimer)
+        
+        if durationTimer == 0 {
+            timer.invalidate()
+        }
+    }
+    //Animation
+    func animationCircular() {
+        
+        let center = CGPoint(x:  shapeView.frame.width / 2, y: shapeView.frame.height / 2)
+        
+        let endAngle = (-CGFloat.pi / 2)
+        let startAngle = 2 * CGFloat.pi + endAngle
 
+        let circularPath = UIBezierPath(arcCenter: center , radius: 138, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.lineWidth = 60
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = 1
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
+        shapeLayer.strokeColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).cgColor
+        shapeView.layer.addSublayer(shapeLayer)
+    }
+    
+    func basicAnimation() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 0
+        basicAnimation.duration = CFTimeInterval(durationTimer)
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = true
+        shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+    }
+}
+extension ViewController {
+    
+    func setConstraints() {
+        
+        view.addSubview(lessonLabel)
+        NSLayoutConstraint.activate([
+            lessonLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            lessonLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 150),
+            lessonLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        view.addSubview(shapeView)
+        NSLayoutConstraint.activate([
+            shapeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shapeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            shapeView.heightAnchor.constraint(equalToConstant: 300),
+            shapeView.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        view.addSubview(timerLabel)
+        NSLayoutConstraint.activate([
+            timerLabel.centerXAnchor.constraint(equalTo: shapeView.centerXAnchor),
+            timerLabel.centerYAnchor.constraint(equalTo: shapeView.centerYAnchor)
+            ])
+        
+        view.addSubview(startButton)
+        NSLayoutConstraint.activate([
+            startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.heightAnchor.constraint(equalToConstant: 70),
+            startButton.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        
+    }
+}
